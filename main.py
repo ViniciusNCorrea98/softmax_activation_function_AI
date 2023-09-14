189d474f56a59ce2f32bebf94a8023bd876b522e
Administrador <viniciuscorrea417@gmail.com>
2023-09-14 15:27:13 -0300

    Softmax Activation Function - Artificial Intelligence

---
+++ main.py
@@ -0,0 +1,62 @@
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
+nnfs.init()
+
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
+
+X, y = spiral_data(100, 3)
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 1.0*np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+class Activation_Regression_Linear:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2, 3)
+activation1 = Activation_Regression_Linear()
+
+dense2= Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+dense1.forward(X)
+
+activation1.forward(dense1.output)
+print(activation1.output)
+
+dense2.forward((activation1.output))
+activation2.forward(dense2.output)
+
+print(activation2.output)
+
+
+
+
+'''
+layer_outpus = [[4.8, 1.21, 2.385],
+                [8.9, -1.81, 0.2],
+                [1.41, 1.051, 0.026]]
+
+
+
+print(exp_values);
+
+norm_values = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+print(norm_values)
+'''
